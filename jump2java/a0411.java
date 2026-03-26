4a4c88b2176e6ff43f5ff53a636946cd37b6e216
kiwimon1 <dj20250610@dge.go.kr>
2026-03-26 14:36:15 +0900

    Create a0411.java

---
+++ jump2java/a0411.java
@@ -0,0 +1,13 @@
+package jump2java;
+
+public class a0501 {
+    public static void main(String[] args) {
+    	for(int i=2; i<10; i++) {
+    	    for(int j=1; j<10; j++) {
+    	        System.out.print(i*j+" ");
+    	    }
+    	    System.out.println("");  // 줄을 바꾸어 출력하는 역할을 한다.
+    	}
+      
+    }
+}
