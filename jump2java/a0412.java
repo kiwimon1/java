0425f78343df5821f45ebb153aed04a4f162ec86
kiwimon1 <dj20250610@dge.go.kr>
2026-03-26 15:01:25 +0900

    Create a0412.java

---
+++ jump2java/a0412.java
@@ -0,0 +1,17 @@
+package jump2java;
+
+import java.util.ArrayList;
+
+public class a0501 {
+    public static void main(String[] args) {
+        ArrayList<String> numbers = new ArrayList<>();
+        numbers.add("one");
+        numbers.add("two");
+        numbers.add("three");
+        for (String number : numbers) {
+            System.out.println(number);
+        }
+    }
+}
+
+        
